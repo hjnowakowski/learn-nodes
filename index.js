df5ac8e555e592965d49c291709820d4aaf3f216
henryknowakowski <DZ5335516GWhj+n7991>
2017-07-01 16:50:49 +0200

    del png

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('yo yo ');
+const del = require('del');
+
+del(['img/*.png']).then(function (paths){ //! znaczy nie usuwaj
+    console.log('Deleted files and folders:\n', paths.join('\n'));
+});
